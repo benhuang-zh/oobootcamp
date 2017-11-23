2187cde33af2b2a31ea4444a1718de702c77fb44
Ben Huang <ben.huang@oocl.com>
2017-11-23 16:08:30 +0800

    print sorted parkables

--- src/main/java/com/oobootcamp/ParkingManager.java
+++ src/main/java/com/oobootcamp/ParkingManager.java
@@ -34,7 +34,9 @@ public class ParkingManager {
     public ReportData getReportData() {
         Integer totalParkedCars = parkables.stream().map(p -> (p.totalCar())).reduce(0, Integer::sum);
         Integer totalAvailableSpaces = parkables.stream().map(p -> (p.availableSpace())).reduce(0, Integer::sum);
-        return ReportDataGenerator.genReportData("M", totalParkedCars, totalAvailableSpaces, parkables);
+        List<Parkable> list = new ArrayList<>(parkables);
+        list.sort((lot1, lot2) -> lot1 instanceof Parkinglot ? (lot2 instanceof Parkinglot ? 0 : -1) : 1);
+        return ReportDataGenerator.genReportData("M", totalParkedCars, totalAvailableSpaces, list);
     }
 
 
--- src/test/java/com/oobootcamp/ParkingDirectorTest.java
+++ src/test/java/com/oobootcamp/ParkingDirectorTest.java
@@ -20,6 +20,20 @@ public class ParkingDirectorTest {
     }
 
     @Test
+    public void can_report_parked_cars_from_parking_manager_when_parking_manager_manages_one_parking_lot_wiht_3_capacity_and_one_car_parked() throws NoSpaceException {
+        String carNumber = "12312";
+        Parkable parkinglot = new Parkinglot(3);
+        parkinglot.parking(new Car(carNumber));
+        ParkingManager parkingManager = new ParkingManager(parkinglot);
+        ParkingDirector parkingDirector = new ParkingDirector(parkingManager);
+        String report = parkingDirector.printReport();
+        String expectedReport = "M 1 2\n" +
+                " P 1 2";
+
+        assertEquals(expectedReport, report);
+    }
+
+    @Test
     public void can_report_parked_cars_from_parking_manager_when_parking_manager_manages_one_parking_boy_and_one_car_parked() throws NoSpaceException {
         ParkingDirector parkingDirector = new ParkingDirector(new ParkingManager(ParkingLots.getParkBoyWithOneCarParked()));
 
@@ -44,19 +58,17 @@ public class ParkingDirectorTest {
         assertEquals(expectedReport, report);
     }
 
-    @Test
-    public void can_park_a_car_in_parking_lot2() throws NoSpaceException {
-        String carNumber = "12312";
-        Parkable parkinglot = new Parkinglot(3);
-        parkinglot.parking(new Car(carNumber));
-        ParkingManager parkingManager = new ParkingManager(parkinglot);
-        ParkingDirector parkingDirector = new ParkingDirector(parkingManager);
-        String report = parkingDirector.printReport();
-        String expectedReport = "M 1 2\n" +
-                " P 1 2";
 
+    @Test
+    public void should_report_parkinglot_data_first_then_parking_boy_data_when_parking_manager_manages__parking_boy_and_parkinglot() throws NoSpaceException {
+        ParkingDirector parkingDirector = new ParkingDirector(new ParkingManager(ParkingLots.getParkBoyWithOneCarParked(),ParkingLots.getParkableWithOneCarParked()));
+
+        String report = parkingDirector.printReport();
+
+        String expectedReport = "M 2 0\n" +
+                " P 1 0\n" +
+                " S 1 0\n" +
+                "  P 1 0";
         assertEquals(expectedReport, report);
     }
-
-
 }
